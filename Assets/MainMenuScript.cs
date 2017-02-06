4dc496e64bbc5f0fecf8c0457277f55edebc9837
Eric A. Soppi <soppi.2@osu.edu>
2017-02-06 01:44:00 -0500

    Basic main menu and coin game scenes added to game

---
+++ Assets/MainMenuScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuScript : MonoBehaviour
+{
+
+    public Canvas quitMenu;
+    public Button startText;
+    public Button exitText;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    quitMenu = quitMenu.GetComponent<Canvas>();
+	    startText = startText.GetComponent<Button>();
+	    exitText = exitText.GetComponent<Button>();
+	    quitMenu.enabled = false;
+	}
+
+    public void ExitPressed()
+    {
+        quitMenu.enabled = true;
+        startText.enabled = false;
+        exitText.enabled = false;
+    }
+
+    public void NoPressed()
+    {
+        quitMenu.enabled = false;
+        startText.enabled = true;
+        exitText.enabled = true;
+    }
+
+    public void PlayPressed()
+    {
+        SceneManager.LoadScene("CoinGame");
+    }
+
+    public void YesPressed()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
---
+++ Assets/PauseScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseScript : MonoBehaviour
+{
+
+    public Canvas pauseMenu;
+    public Button pauseText;
+
+	void Start ()
+	{
+	    pauseMenu = pauseMenu.GetComponent<Canvas>();
+	    pauseText = pauseText.GetComponent<Button>();
+	    pauseMenu.enabled = false;
+	}
+
+    public void PausePressed()
+    {
+        pauseMenu.enabled = true;
+        pauseText.enabled = false;
+    }
+
+    public void ResumePressed()
+    {
+        pauseMenu.enabled = false;
+        pauseText.enabled = true;
+    }
+
+    public void GoToMainPressed()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
