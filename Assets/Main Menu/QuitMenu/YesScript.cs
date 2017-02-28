481875605217adb046c589d4579327666c8f0b28
Eric A. Soppi <soppi.2@osu.edu>
2017-02-27 20:31:02 -0500

    Broke the main menu into separate scripts so that we can utilize Unity interfaces

---
+++ Assets/Main Menu/QuitMenu/YesScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class YesScript : MonoBehaviour, IPointerEnterHandler
+{
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        YesPressed();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        YesPressed();
+    }
+
+    private void YesPressed()
+    {
+        Debug.Log("Main Menu: YesPressed()");
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
