9ad247477849ce1c3dbba05541bb808997f623fc
nathan krebs <nathan.krebs@duethealth.com>
2017-04-07 15:34:59 -0400

    fix me

--- Assets/Coin Game/Scripts/CoinGameManager.cs
+++ Assets/Coin Game/Scripts/CoinGameManager.cs
@@ -1,20 +1,76 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using Kinect;
+using UnityEngine.AI;
 
 public class CoinGameManager : MonoBehaviour, IManager
 {
 	public static CoinGameManager singleton { get; private set; }
 
-    public GameObject skeleton;
-    private GameState currentState;
-    public enum GameState { NewGame, GameRunning, GameStopped, Countdown }
+	public GameObject skeleton;
+	private GameState currentState;
 
-	private CoinGameManager(){}
+	public enum GameState
+	{
+		NewGame,
+		GameRunning,
+		GameStopped,
+		Countdown
+
+	}
+
+	private CoinGameManager ()
+	{
+	}
 
 	void Start ()
 	{
 		singleton = this;
 		Initialize ();
-		Debug.Log("Coin Game Scene Entered");
+		TranslateCollisionBoxes ();
+		Debug.Log ("Coin Game Scene Entered");
+
+	}
+
+	void TranslateCollisionBoxes ()
+	{
+		Renderer r = this.gameObject.AddComponent<MeshRenderer> ();
+		for (int i = 0; i < this.transform.childCount; i++) {
+			
+			Transform child = this.transform.GetChild (i);
+			if (child.name != "Start") {
+				continue;
+			}
+			Debug.Log ("fuck shit");
+			var rect = child.GetComponent<RectTransform> ();
+			Vector2 anchorPos = rect.anchoredPosition;
+
+			float camHeight = 2f * Camera.main.orthographicSize;
+			float camWidth = camHeight * Camera.main.aspect;
+
+			Debug.Log (r.bounds.size);
+
+			float translatedX = (anchorPos.x / r.bounds.size.x) * camWidth;
+			float translatedY = (anchorPos.y / r.bounds.size.y) * camHeight;
+			float translatedWidth = (rect.rect.width / r.bounds.size.x) * camWidth;
+			float translatedHeight = (rect.rect.height / r.bounds.size.y) * camHeight;
+			Debug.Log (translatedX);
+			Debug.Log (translatedY);
+			Debug.Log (translatedWidth);
+			Debug.Log (translatedHeight);
+
+			var collider = child.GetComponent<BoxCollider> ();
+			collider.bounds.SetMinMax (
+				new Vector3 (translatedX, translatedY, -10000),
+				new Vector3 (translatedX + translatedWidth, translatedY + translatedHeight, 100000));
+
+			collider.bounds.SetMinMax (
+				new Vector3 (0, 0, 0),
+				new Vector3 (5, 5, 5));
+			//collider.bounds.center = new Vector3 (translatedX + translatedWidth / 2, translatedY + translatedHeight / 2, 0);
+			//collider.bounds.size = new Vector3 (translatedWidth, translatedHeight, 10000);
+
+		}
 	}
 
 	void Update ()
@@ -26,33 +82,33 @@ public class CoinGameManager : MonoBehaviour, IManager
 		}
 	}
 
-	public void SetStateToStopped()
+	public void SetStateToStopped ()
 	{
 		currentState = GameState.GameStopped;
 	}
 
-	public void SetStateToRunning()
+	public void SetStateToRunning ()
 	{
 		currentState = GameState.GameRunning;
 	}
 
-	public void SetStateToNew()
+	public void SetStateToNew ()
 	{
 		currentState = GameState.NewGame;
 	}
 
-	public void SetStateToCountdown()
+	public void SetStateToCountdown ()
 	{
 		currentState = GameState.Countdown;
 	}
 
 
-	public GameState GetGameState()
+	public GameState GetGameState ()
 	{
 		return currentState;
 	}
 
-	public void Initialize()
+	public void Initialize ()
 	{
 		SetStateToNew ();
 	}
---
+++ Assets/Coin Game/Scripts/CoinGameUIManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinGameUIManager: MonoBehaviour
+{
+
+	void Start ()
+	{
+		GameObject canvasGo = new GameObject ();
+		canvasGo.transform.parent = transform;
+		canvasGo.name = "canvasGo";
+		RectTransform canvasRT = canvasGo.AddComponent<RectTransform> ();
+		Canvas canvasCV = canvasGo.AddComponent<Canvas> ();
+		canvasCV.renderMode = RenderMode.ScreenSpaceCamera;
+//		Vector3 pos = Camera.main.transform.position;
+//		pos += Camera.main.transform.forward * 10f;
+		canvasCV.worldCamera = Camera.main;
+
+		GameObject buttonGo = new GameObject ();
+		RectTransform buttonRT = buttonGo.AddComponent<RectTransform> ();
+		buttonRT.SetParent (canvasRT);
+		buttonRT.sizeDelta = new Vector2 (200f, 100f);
+		Button buttonBu = buttonGo.AddComponent<Button> ();
+		buttonBu.gameObject.name = "buttonBu";
+
+		buttonBu.onClick.AddListener (() => {
+			Debug.Log ("button clicked");
+		});
+	}
+}
+
+
